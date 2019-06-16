607a73f6e5d8370fd0be77d73ff47f5ace27915c
Romain Codazzi <rcodazzi@z3r7p3.le-101.fr>
2019-06-16 14:35:05 +0200

    header+css new

--- header.php
+++ header.php
@@ -13,51 +13,22 @@
 <body>
 
 
-<header style="background-color: grey; width:100%;">
+<header>
+<h1 class="deco">Chocolatte</h1>
+<nav>
+    <ul>
+        <li><a href="~/index.php">vitrine</li>
+        <?php //if (session visiteur) ?>
+        <li><a href="./loginSystem/login.php">Log in</a></li>
+        <li><a href="./loginSystem/signUp.php">Sign in</a></li>
 
-<!-- <div>
-    <h1><a href="~/index.php">Camagru</a></h1>
+        <li><a href="./userSession/lpanier.php">panier</a></li>
+        <?php //if (user/admin log) ?>
+        <li><a href="./userSession/settings.php">Setting</a></li>
+        <li><a href="~/index.php">disconnect</a></li>
+    </ul>
+</nav>
 
-    <?php //if () Personne de log dans index ?>
-    <nav>
-        <a href="./loginSystem/login.php">Login</a>
-        <a href="./loginSystem/signUp.php">Sign up</a>
-    </nav>
-    
-     <?php //if () Users dans index ?>
-        <a href="./userSession/lpanier.php">Basket</a>
-        <a href="./loginSystem/signUp.php">Disconnect</a>
-    <?php //if () Admin log ?>
-        <a href="./userSession/lpanier.php">Basket</a>
-        <a href="./loginSystem/signUp.php">Disconnect</a>
-        <a href="./admin/index_admin.php">Gestion</a> 
-
-</div> -->
-<div>
-    <h1><a href="~/index.php">Camagru</a></h1>
-                <div>
-                <ul class="menu">
-                    <li style="text-align: center;">shop</li>
-                    <li><a href="~/index.php">vitrine</a></li>
-                    <li><a href="./userSession/lpanier.php">panier</a></li>
-                </ul>
-                <ul class="menu">
-                    <li style="text-align: center;">Utilisateur</li>
-                    <?php //if () user pas connecter  ?>
-                        <li><a href="./loginSystem/login.php">Log in</a></li>
-                        <li><a href="./loginSystem/signUp.php">Sign in</a></li>
-                        <?php //else ?>
-                        <li><a href="./userSession/settings.php">Setting</a></li>
-                        <li><a href="~/index.php">disconnect</a></li> <?php // deconnecter user?>
-                    
-                </ul>
-                <?php  //if () admin co ?>
-                <ul class="menu">
-                    <li style="text-align: center;">Gestion Admin</li>
-                    <li><a href="./admin/index_admin.php">gestion</a></li>
-                </ul>
-                </div>
-</div>
 
 </header>
 </html>
