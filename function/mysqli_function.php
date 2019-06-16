fdef600a309b5927062fa83dd7173422ca1d434f
Vivien De saint-exupery <vde-sain@z3r6p2.le-101.fr>
2019-06-16 09:44:03 +0200

    working on admin area

---
+++ function/mysqli_function.php
@@ -0,0 +1,62 @@
+<?PHP
+
+function mysqli_shutdown($stmt, $mysqli)
+{
+	mysqli_stmt_close($stmt);
+	if (mysqli_errno($mysqli))
+		die("Error4 : " . mysqli_stmt_error($stmt));
+	mysqli_close($mysqli);
+}
+
+function mysqli_open()
+{
+	$mysqli = mysqli_connect("mysql", "root", "rootpass", "rush");
+	if (!$mysqli)
+	{
+		echo "Erreur : Impossible de se connecter à MySQL." . PHP_EOL;
+		echo "Errno de débogage : " . mysqli_connect_errno() . PHP_EOL;
+		echo "Erreur de débogage : " . mysqli_connect_error() . PHP_EOL;
+		exit;
+	}
+	return $mysqli;
+}
+
+/////////////////////////////////// ADMIN
+
+function create_product()
+{
+	$mysqli = mysqli_open();
+	$query = "SELECT `product_name` FROM `products` WHERE `product_name` = ?  ";
+	$product = $_POST['name_product'];
+	if (($stmt = mysqli_prepare($mysqli, $query)) === FALSE)
+		die("Error1 : " . mysqli_error($mysqli));
+	if (mysqli_stmt_bind_param($stmt, "s", $product) === FALSE)
+		die("Error2 : " . mysqli_stmt_error($stmt));
+	if (mysqli_stmt_execute($stmt) === FALSE)
+		die("Error3 : " . mysqli_stmt_error($stmt));
+	if (mysqli_stmt_bind_result($stmt, $sql_name_product) === FALSE)
+		die("Error4 : " . mysqli_stmt_error($stmt));
+	if (mysqli_stmt_fetch($stmt))
+	{
+		echo "<br>Product already exists";
+		mysqli_shutdown($stmt, $mysqli);
+	}
+	else
+	{
+		mysqli_shutdown($stmt, $mysqli);
+		$mysqli = mysqli_connect("mysql", "root", "rootpass", "rush");
+		$query = "INSERT INTO `products`(`product_name`, `path`, `price`, `left`, `category`) VALUE(?, ?, ?, ?, ?)";
+		if (($stmt = mysqli_prepare($mysqli, $query)) === FALSE) {
+			die("Error1 : " . mysqli_error($mysqli));
+		}
+		if (mysqli_stmt_bind_param($stmt, "ssdds", $_POST['name_product'], $_POST['photo'], $_POST['prix'], $_POST['stock'], $_POST['category']) === false) {
+			die("Error2 : " . mysqli_stmt_error($stmt));
+		}
+		if (mysqli_stmt_execute($stmt) === false) {
+			die("Error3 : " . mysqli_stmt_error($stmt));
+		}
+		mysqli_shutdown($stmt, $mysqli);
+		echo "<br/>Product successfully added to database";
+	}
+}
+?>
